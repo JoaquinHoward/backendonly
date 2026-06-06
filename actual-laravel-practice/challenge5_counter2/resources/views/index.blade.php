5127621190005c195ffd81a2d1424b93c1110a25
JoaquinHoward <falarjoaquinhoward@gmail.com>
2026-06-07 00:52:00 +0800

    Practiced counter.

---
+++ actual-laravel-practice/challenge5_counter2/resources/views/index.blade.php
@@ -0,0 +1,4 @@
+<h1>You have visited the site {{ $views }} times.</h1>
+<form action = "/reset" method = "GET">
+    <input type = "submit" value = "reset">
+</form>
---
+++ actual-laravel-practice/challenge5_counter2/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CounterController;
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get("/", [CounterController::class, "increment"]);
+Route::get("/reset", [CounterController::class, "reset"]);
